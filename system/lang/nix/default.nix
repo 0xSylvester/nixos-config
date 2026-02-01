96d1aa01fb68e8bc5e67336eb262eb3af97e5706
sylvester <mhxxx2005@gmail.com>
2026-02-01 15:26:11 +0200

    Add nix lsp

--- system/lang/nix/default.nix
+++ system/lang/nix/default.nix
@@ -8,7 +8,7 @@ in {
   };
 
   config = lib.mkIf cfg.enable {
-    environment.systemPackages = with pkgs; [ nixfmt-classic ];
+    environment.systemPackages = with pkgs; [ nixfmt nil ];
   };
 }
 
