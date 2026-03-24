fec1cd400efa98fba1325432a03bdcfe29c77f8c
sylvester <mhxxx2005@gmail.com>
2026-03-25 01:13:21 +0200

    Add some default program via xdg-mime

--- user/xdg/default.nix
+++ user/xdg/default.nix
@@ -9,6 +9,49 @@ in {
   };
 
   config = lib.mkIf cfg.enable {
+    xdg.mimeApps = {
+      enable = true;
+      defaultApplications = {
+        "text/plain" = [ "doom.desktoop" ];
+        "text/org" = [ "doom.desktoop" ];
+        "text/markdown" = [ "doom.desktop" ];
+        "text/css" = [ "doom.desktop" ];
+        "text/html" = [ "doom.desktop" ];
+        "text/js" = [ "doom.desktop" ];
+        "text/vue" = [ "doom.desktop" ];
+        "text/go" = [ "doom.desktop" ];
+        "text/c" = [ "doom.desktop" ];
+        "text/cpp" = [ "doom.desktop" ];
+        "text/python" = [ "doom.desktop" ];
+        "text/rust" = [ "doom.desktoop" ];
+        "text/x-typst" = [ "doom.desktoop" ];
 
+        "application/xml" = [ "doom.desktop" ];
+        "application/json" = [ "doom.desktop" ];
+        "application/yaml" = [ "doom.desktop" ];
+        "application/toml" = [ "doom.desktop" ];
+
+        "application/pdf" = [ "org.pwmt.zathura.desktop" ];
+
+        "image/png" = [ "imv.desktop" ];
+        "image/svg" = [ "imv.desktop" ];
+        "image/jpeg" = [ "imv.desktop" ];
+        "image/gif" = [ "imv.desktop" ];
+
+        "video/mp4" = [ "mpv.desktop" ];
+        "audio/mp3" = [ "mpv.desktop" ];
+      };
+    };
+    xdg.desktopEntries.doom = {
+      name = "DOOM";
+      genericName = "Text Editor";
+      comment = "Edit text files using the Doom Emacs ";
+      exec = "emacsclient -c -a emacs %F";
+      terminal = false;
+      icon = "emacs";
+      categories = [ "Development" "TextEditor" ];
+      mimeType = [ "text/plain" "text/rust" "text/org" ];
+      settings = { Keywords = "Text;Editor;Write;Programming;"; };
+    };
   };
 }
